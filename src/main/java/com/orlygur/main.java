922af181e0c69a4854997d429eff1e391b16955c
orlygurhugason <orlygurhugason@localhost>
2024-05-04 20:18:37 +0000

    made the commands work

--- src/main/java/com/orlygur/main.java
+++ src/main/java/com/orlygur/main.java
@@ -7,26 +7,41 @@ public class main {
 	public static void main(String[] args) {
 		Scanner Command = new Scanner(System.in);
 
-		PrintG("Hello and welcome to Sysvac. \n"
+		PrintG("Hello and welcome to Sysvac. Beta \n"
 		+ "Please type a command \n"
 		+ "Type Help for basic commands \n");
-		
-		String CMD = Command.nextLine();
-		String OP = CMD.toLowerCase();
-		
-		Command(OP);
-		
-		CMD = Command.nextLine();
-		OP = CMD.toLowerCase();
+		while (true) {
+			  input();
+		}
 	}
 	
 	static void PrintG(String text) {
 		System.out.println("\u001B[32;1m" + text + "\u001B[0m");
-	}
+	} // Prints green text
+	
 	static void Command(String text) {
 		if (text.equals("help")) {
 			PrintG("\n" 
 			+ "The commands are in progress \n");
+		} else if (text.equals("exit")) {
+			PrintG("\n" 
+					+ "Exiting. \n");
+			System.exit(0);
+		} else {
+			PrintG("\n"
+					+ "Not a Command \n");
 		}
-	}
+	} // checks which command was typed
+	
+	static void input() {
+		Scanner Command = new Scanner(System.in);
+		String CMD;
+		String OP;
+		
+		CMD = Command.nextLine();
+		OP = CMD.toLowerCase();
+		
+		Command(OP);
+	} // takes user input
+	
 }
